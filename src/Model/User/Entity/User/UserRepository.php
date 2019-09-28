4b659d4d3f7533bfa0b1e3a27df8cfe37ef028f9
Nail Bagautdinov <bagnail@mail.ru>
2019-09-28 11:44:34 +0300

    Implemented UserRepository methods

--- src/Model/User/Entity/User/UserRepository.php
+++ src/Model/User/Entity/User/UserRepository.php
@@ -3,28 +3,63 @@
 namespace App\Model\User\Entity\User;
 
 
+use App\Model\EntityNotFoundException;
+use Doctrine\ORM\EntityManagerInterface;
+
 class UserRepository
 {
+
+    private $em;
+    /**
+     * @var \Doctrine\ORM\EntityRepository
+     */
+    private $repo;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->repo = $em->getRepository(User::class);
+    }
+
     public function findByConfirmToken(string $token): ?User
     {
-
+        $this->repo->findOneBy(['confirmToken' => $token]);
     }
+
     public function findByResetToken(string $token): ?User
     {
-
+        $this->repo->findOneBy(['resetToken.token' => $token]);
     }
+
+    public function get(Id $id): User
+    {
+        /** @var User $user */
+        if (!$user = $this->repo->find($id->getValue())) {
+            throw new EntityNotFoundException('User is not found.');
+        }
+        return $user;
+    }
+
     public function getByEmail(Email $email): User
     {
-
+        /** @var User $user */
+        if (!$user = $this->repo->findOneBy(['email' => $email->getValue()])) {
+            throw new EntityNotFoundException('User is not found.');
+        }
+        return $user;
     }
 
     public function hasByEmail(Email $email): bool
     {
-
+        return $this->repo->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->andWhere('t.email = :email')
+                ->setParameter(':email', $email->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
     }
 
     public function add(User $user):void
     {
-
+        $this->em->persist($user);
     }
 }
